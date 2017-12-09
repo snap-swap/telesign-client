cc6177bceafbe6a96d56c9c30b2a7b2b762930a5
Dmitry Smirnov <dmitry.smirnov.84@gmail.com>
2017-12-09 15:30:58 +0300

    pretty toString for TelesignPhoneScore

--- src/main/scala/com/snapswap/telesign/model/external/TelesignPhoneScore.scala
+++ src/main/scala/com/snapswap/telesign/model/external/TelesignPhoneScore.scala
@@ -4,10 +4,22 @@ import java.time.ZonedDateTime
 
 import com.snapswap.telesign.model.external.RiskLevelEnum.RiskLevel
 
+
 case class TelesignPhoneScore(phone: String,
                               riskLevel: RiskLevel,
                               riskScore: Int,
                               phoneType: EnumPhoneTypes.PhoneType,
                               carrier: String,
                               updatedOn: ZonedDateTime,
-                              iso3CountryCode: String)
+                              iso3CountryCode: String) {
+  override def toString: String =
+    s"""
+       |phone = $phone,
+       |riskLevel = $riskLevel,
+       |riskScore = $riskScore,
+       |phoneType = $phoneType,
+       |carrier = $carrier,
+       |updatedOn = $updatedOn,
+       |iso3CountryCode = $iso3CountryCode
+     """.stripMargin
+}
